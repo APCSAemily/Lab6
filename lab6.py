b7e340f22db1523c48f5dadc8561c58b9ed58fbf
Christian Domingo <cdomm14@gmail.com>
2024-10-22 16:58:58 -0400

    Added decode() function and menu option 2

--- lab6.py
+++ lab6.py
@@ -6,6 +6,20 @@ def encode(original):
         encoded.append(str((int(letter) + 3) % 10))
     return "".join(encoded)
 
+# decode function added by Christian Domingo
+def decode(enc):
+    decoded = ""
+    temp_list = list(enc)
+    for i in range(len(enc)):
+        if temp_list[i] == "0":
+            decoded += "7"
+        elif temp_list[i] == "1":
+            decoded += "8"
+        elif temp_list[i] == "2":
+            decoded += "9"
+        else:
+            decoded += str(int(temp_list[i]) - 3)
+    return decoded
 
 def main():
     is_running = True
@@ -22,6 +36,10 @@ def main():
             password = input("Please enter your password to encode:")
             enc = encode(password)
             print("Your password has been encoded and stored!\n")
+        # menu option 2 added by Christian Domingo
+        elif option == 2:
+            dec = decode(enc)
+            print(f"The encoded password is {enc}, and the original password is {dec}")
         if option == 3:
             is_running = False
 
