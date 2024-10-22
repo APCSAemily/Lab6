da9ec81eea993a1c722c3c20296ef6c99b94f239
APCSAemily <emilyyqhuang@gmail.com>
2024-10-22 11:55:26 -0500

    Encoded Function

---
+++ lab6.py
@@ -0,0 +1,26 @@
+#Original File: Yiqiao (Emily) Huang
+
+def encode(original):
+    encoded = []
+    for letter in original:
+        encoded.append(str((int(letter) + 3) % 10))
+    return "".join(encoded)
+
+
+def main():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print("")
+
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        password = input("Please enter your password to encode:")
+        enc = encode(password)
+        print("Your password has been encoded and stored!\n")
+
+
+if __name__ == "__main__":
+    main()
