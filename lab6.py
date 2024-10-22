03b6187b6e0c1a5aec8e2f9b1b2a30257c607cd7
APCSAemily <emilyyqhuang@gmail.com>
2024-10-22 12:30:52 -0500

    main() function updated loop.

--- lab6.py
+++ lab6.py
@@ -8,18 +8,22 @@ def encode(original):
 
 
 def main():
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
-    print("")
+    is_running = True
+    while is_running:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print("")
 
-    option = int(input("Please enter an option: "))
-    if option == 1:
-        password = input("Please enter your password to encode:")
-        enc = encode(password)
-        print("Your password has been encoded and stored!\n")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode:")
+            enc = encode(password)
+            print("Your password has been encoded and stored!\n")
+        if option == 3:
+            is_running = False
 
 
 if __name__ == "__main__":
